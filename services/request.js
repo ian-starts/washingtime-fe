e2d2da47a7d1a249af11c8a3b9bc5fcd2448e9e3
Yoni Ian Kok <hello@yik.dev>
2020-03-31 12:03:20 +0200

    add https

--- services/request.js
+++ services/request.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const getClient = (token) => axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://api.wannawash.nl/api',
     timeout: 60000,
     headers: {
         authorization: 'Bearer ' + token
